75ba3a71554a51489172e78530c4d1b0b94e036f
Ramagil <honibusa2008@gmail.com>
2026-04-25 22:20:56 +0700

    tampilan kanidat

--- script/script.js
+++ script/script.js
@@ -1,27 +1,7 @@
-const buttons = document.querySelectorAll(".btn-profile");
-const popup = document.getElementById("popup");
-const closeBtn = document.querySelector(".close");
+function openPopup(id) {
+  document.getElementById(id).style.display = "block";
+}
 
-const popupImg = document.getElementById("popup-img");
-const popupTitle = document.getElementById("popup-title");
-const popupDesc = document.getElementById("popup-desc");
-
-buttons.forEach(btn => {
-  btn.addEventListener("click", () => {
-    popup.style.display = "block";
-
-    popupImg.src = btn.getAttribute("data-img");
-    popupTitle.innerText = btn.getAttribute("data-nama");
-    popupDesc.innerText = btn.getAttribute("data-desc");
-  });
-});
-
-closeBtn.addEventListener("click", () => {
-  popup.style.display = "none";
-});
-
-window.addEventListener("click", (e) => {
-  if (e.target == popup) {
-    popup.style.display = "none";
-  }
-});
+function closePopup(id) {
+  document.getElementById(id).style.display = "none";
+}
