c8ff55a435d75f138e4c99f55eb093691654d0b9
FelicyaAgatha <felicyafelicya09@gmail.com>
2026-05-06 10:02:21 +0700

    update

--- view/login.php
+++ view/login.php
@@ -6,6 +6,7 @@ include "../config/koneksi.php";
 <!DOCTYPE html>
 <html lang="id">
 <head>
+  
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login E-Voting</title>
